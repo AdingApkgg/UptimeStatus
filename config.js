d0d6267ebf2a4dc2027c8487c70d5fb75f7da5e4
AdingApkgg <AdingApkgg@outlook.com>
2024-04-22 20:44:31 +0800

    .

--- config.js
+++ config.js
@@ -8,12 +8,10 @@ window.Config = {
   ApiKeys: [
     'm794516267-b55a494a66ce8946f6f70b8e', // 小站
     'm794516281-a8d97dc5043cf8cd79dc7d29', // 博客
-    'm796712700-a2e283d85258db32240f75a5', // Gal资源索引
-	'm794516507-e544fee64a0fd497546fa68b', // 站点监控
-    'm794516318-f3aaa1a73d10ad2ea6851a30', // 原神群推荐
-	'm794516304-983c3d13496d77bc85b0f962', // Google镜像
-	'm794516311-1a0aa7172e575f7230a859bd', // ChatGPT镜像
-	'm794600345-3c7861497fe4b8d3c58919e6', // 随机图片API
+    'm796712700-a2e283d85258db32240f75a5', // Gal索引
+    'm794516318-f3aaa1a73d10ad2ea6851a30', // 原神BBS
+	'm794516304-983c3d13496d77bc85b0f962', // 谷歌镜像
+	'm794600345-3c7861497fe4b8d3c58919e6', // 随机图片
   ],
 
   // 日志天数
@@ -25,16 +23,8 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: '主页',
+      text: '定の栈',
       url: 'https://saop.cc'
-    },
-    {
-      text: '博客',
-      url: 'https://blog.saop.cc/'
-    },
-	{
-      text: 'GitHub',
-      url: 'https://github.com/AdingApkgg'
     },
   ],
 };
