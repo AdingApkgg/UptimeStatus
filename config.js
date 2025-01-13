26875f97de259effe8cd5420255aa16eac3cfeaa
AdingApkgg <AdingApkgg@outlook.com>
2025-01-14 02:09:23 +0800

    .

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'AsunaStatus',
+  SiteName: 'Asuna Status',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm794516267-b55a494a66ce8946f6f70b8e', // Home
-    'm794516281-80106b0d4ad31ff848cf1331', // Blog
+    'm794516267-b55a494a66ce8946f6f70b8e', // Asuna Home
+    'm794516281-80106b0d4ad31ff848cf1331', // Asuna Blog
     'm796712700-a2e283d85258db32240f75a5', // GGS
     'm794516318-f3aaa1a73d10ad2ea6851a30', // Genshin Impact BBS
-	'm798344797-1fc1b79bb3bc61ae8cb51012', // AList
-	'm798344799-0aa13f17e3c83ef245fded05', // Cloudreve
+	'm798344917-9fbf5b49ac8e17840d2ddcb9', // Asuna Drive
 	'm794600345-3c7861497fe4b8d3c58919e6', // Random pictures
   ],
 
